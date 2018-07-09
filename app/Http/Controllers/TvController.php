0f0f730255fe0dd91b24ea06bb98d3847f6edccc
António Lima <limaa91@gmail.com>
2018-07-09 17:18:26 +0100

    refactoring.

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -12,45 +12,6 @@ class TvController extends Controller
     protected $dataScraper;
 
     public function index(){
-        return redirect('tv/on-the-air');
-    }
-
-    public function onTheAir()
-    {
-        $dataScraper = new DataScraper();
-        $showsDataArray = $dataScraper->getOnTheAir(5);
-        //dd($showsDataArray);
-        $headerLink = 'On The Air';
-        return view('tv.on-the-air', compact('showsDataArray', 'headerLink'));
-    }
-
-    public function airingToday(){
-        $dataScraper = new DataScraper();
-        $showsDataArray = $dataScraper->getAiringToday(5);
-        //dd($showsDataArray);
-        $headerLink = 'Airing Today';
-        return view('tv.on-the-air', compact('showsDataArray', 'headerLink'));
-    }
-
-    public function popular(){
-    $dataScraper = new DataScraper();
-    $showsDataArray = $dataScraper->getPopularShows(5);
-    //dd($showsDataArray);
-    $headerLink = 'Popular';
-    return view('tv.on-the-air', compact('showsDataArray', 'headerLink'));
-}
-
-    public function topRated(){
-        $dataScraper = new DataScraper();
-        $showsDataArray = $dataScraper->getTopRatedShows(5);
-        //dd($showsDataArray);
-        $headerLink = 'Top Rated';
-        return view('tv.on-the-air', compact('showsDataArray', 'headerLink'));
-    }
-
-    public function test(){
         return view('tv.home');
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/home', 'HomeController@home')->name('home');
 
 Route::prefix('tv')->group(function () {
     Route::get('/', 'TvController@index');
-    Route::get('/on-the-air', 'TvController@onTheAir');
-    Route::get('/airing-today', 'TvController@airingToday');
-    Route::get('/popular', 'TvController@popular');
-    Route::get('/top-rated', 'TvController@topRated');
     // test
     Route::get('/test', 'TvController@test');
 });
