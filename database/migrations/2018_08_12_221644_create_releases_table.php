e2b6ea150630d596aa856e88ceeea172a11ce284
António Lima <limaa91@gmail.com>
2018-08-12 23:29:27 +0100

    added new table Release.

---
+++ database/migrations/2018_08_12_221644_create_releases_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReleasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('releases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('moviedb_id');
+            $table->integer('item_type_id');
+            $table->dateTime('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('releases');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -61,6 +62,69 @@ class DatabaseSeeder extends Seeder
                 'user_id' => 2,
                 'description' => 'My watchlist',
                 'removable' => false,
+            ]
+        ]);
+        DB::table('list_items')->insert([
+            [
+                'user_list_id' => 1,
+                'moviedb_id' => 60625,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 67744,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 48866,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 39852,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 60059,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 1415,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 1,
+                'moviedb_id' => 70513,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 2,
+                'moviedb_id' => 70513,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 2,
+                'moviedb_id' => 70513,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],[
+                'user_list_id' => 2,
+                'moviedb_id' => 70513,
+                'item_type_id' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]
         ]);
     }
