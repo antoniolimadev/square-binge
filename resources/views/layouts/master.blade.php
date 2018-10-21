b0f6943de7ae663770ba9c42b3e5e25990c435f9
António Lima <limaa91@gmail.com>
2018-10-21 12:44:05 +0100

    fixed footer.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,23 +8,25 @@
     <link href="{{ url('/') }}/fontawesome-free-5.2.0-web/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    @include('layouts.nav')
-    @if(count($errors))
-        <div>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li> {{ $error }} </li>
-                @endforeach
-            </ul>
+    <div class="main-container">
+        @include('layouts.nav')
+        @if(count($errors))
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li> {{ $error }} </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="content">
+            @yield('content')
         </div>
-    @endif
-    <div class="content">
-        @yield('content')
+        <div style="margin-top: 100px;">
+            <br>
+        </div>
+        @include('layouts.footer')
     </div>
-    <div style="margin-top: 1000px;">
-        space
-    </div>
-    @include('layouts.footer')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="{{ url('/') }}/js/app.js" ></script>
     <script type="text/javascript">
